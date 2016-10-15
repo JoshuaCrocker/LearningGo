a1dac64943a96b4804a2e8a47a51ff36b3b83892
Joshua Crocker <joshua@auratemple.net>
2016-10-15 10:58:49 +0100

    Completed 02_packages

---
+++ 02_packages/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/JoshuaCrocker/LearningGo/02_packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+
+}
