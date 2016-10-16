c499209fa352e661277b40b88561d0480c959bf9
Joshua Crocker <joshua@auratemple.net>
2016-10-16 13:45:25 +0100

    Updated 04_scope / 03_shadowing

--- 04_scope/03_shadowing/main.go
+++ 04_scope/03_shadowing/main.go
@@ -9,6 +9,7 @@ func max(x int) int {
 func main() {
 	max := max(7)
 	fmt.Println(max) // max is now the result, not the function
+	// cannot call function max() from here anymore...
 }
 
 // This is very bad coding practice; don't do this.
